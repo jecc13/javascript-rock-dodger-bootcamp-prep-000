1bc7fd5264d1ee0739231ef1a69e999676fd7ebd
Jennifer E <je@jenniferevenson.com>
2018-01-17 23:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
 //  ROCKS.length = 0;
-  GAME.querySelectorAll(".rocks").remove();
+  document.querySelectorAll(".rocks").remove();
   window.removeEventListener('keydown', moveDodger);
     alert('YOU LOSE!');
 }
