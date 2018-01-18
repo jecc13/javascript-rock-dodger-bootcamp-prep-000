4ac849f04e1ba27167077db2ca539028cb441aab
Jennifer E <je@jenniferevenson.com>
2018-01-18 03:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,12 +141,9 @@ function positionToInteger(p) {
 function start() {
   window.addEventListener('keydown', moveDodger);
   START.style.display = 'none';
-
-/*
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
   }, 1000);
-*/  
 }
 //end end 
 
