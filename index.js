da065fbcf1ade8b4b9753fa7b68e29fe098bd758
Jennifer E <je@jenniferevenson.com>
2018-01-18 02:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -165,43 +165,4 @@ DODGER.style.backgroundColor = 'yellow';
 }
 //end end 
 
-// Simpler code needed to get stuff working before building up to the complex crap.
-
-/* this works to move the rocks, without checking for collisions
-
-function moveRock() {
-    function step() {
-      rock.style.top = `${top += 2}px`;
-      if (top < GAME_HEIGHT) {
-        window.requestAnimationFrame(step);
-      }
-    }
-    window.requestAnimationFrame(step);
-*/
-
-/*
-const jen = document.createElement('div');
-jen.className = 'rock';
-jen.style.left = `${x}px`;
-//jen.style.left = '4px';
-jen.style.top = '2px';
-GAME.appendChild(jen);
-
-function move(jen) {
-  var top = 0;
- 
-  function step() {
-    jen.style.top = `${top += 2}px`;
- 
-    if (top < 400) {
-      window.requestAnimationFrame(step);
-    }
-  }
-  window.requestAnimationFrame(step);
-} // end move(jen)
-
-move(jen);
-ROCKS.push(jen);
-return jen;
-*/
 
