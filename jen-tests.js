eba9b79a5439212b39252e8befb407a0aa162f19
Jennifer E <je@jenniferevenson.com>
2018-01-18 03:42:57 +0000

    automatically backed up by learn

--- jen-tests.js
+++ jen-tests.js
@@ -1,3 +1,5 @@
+/*
+
 To improve your test, I suggest changing your "180px" test case in the checkCollision(rock) function.
 
 If the student's program fails to check for case 2 in the instructions ((rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge)), dropping a rock with a left edge of between 181 and 199, inclusive, will pass the tests and the rock will pass right through the dodger in the game. 
@@ -13,7 +15,7 @@ Before submitting, I went back through the original instructions - which I had b
 
 If you change your "180px" check to something between 181-199, you'll catch the cases where the rock is directly over the dodger with neither edge aligned with the dodger's edges.
 
-
+*/
 
 
 // Simpler code needed to get stuff working before building up to the complex crap.
